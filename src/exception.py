5011eba773b20354ef54334467d1cd454c78f6e7
Abhishek Singh <abhisheksinghbiet@gmail.com>
2024-03-04 22:51:06 +0530

    Logging and exception

---
+++ src/exception.py
@@ -0,0 +1,19 @@
+import sys
+import logging
+import logger
+
+def error_message(error, error_det : sys):
+    _,_,exc_tb = error_det.exc_info()
+    file_name = exc_tb.tb_frame.f_code.co_filename
+    error_msg = "Error occurred in python script [{0}] line number [{1}] \
+        error message [{2}]".format(file_name,exc_tb.tb_lineno,str(error))
+    
+    return error_msg 
+
+class CustomException(Exception):
+    def __init__(self,error_msg,error_det):
+        super().__init__(error_msg)
+        self.error_msg = error_message(error_msg,error_det=error_det)
+
+    def __str__(self) -> str:
+        return self.error_msg     
